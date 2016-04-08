a8fbd5e8ea64724de03f674d2d49fce8797bbbe1
Jonas Vestlund <jonve547@student.liu.se>
2016-04-08 16:01:40 +0200

    The purge

--- elpa/magit-popup-20160329.858/magit-popup-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; magit-popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (22268 11359 87580 469000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-popup-autoloads.el ends here
