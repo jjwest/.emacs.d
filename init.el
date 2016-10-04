3e9bbfcfb8f19ca61e84dc8a6406925193a12b95
Jonas Vestlund <jonve547@student.liu.se>
2016-10-04 23:56:44 +0200

    Gjort shell-command async.

--- init.el
+++ init.el
@@ -44,11 +44,13 @@
       display-time-day-and-date t
       display-time-default-load-average nil
       locale-coding-system 'utf-8)
+
 (prefer-coding-system 'utf-8)
 (setq-default cursor-in-non-selected-windows nil
 	      fill-column 80)
 (put 'narrow-to-region 'disabled nil)
 (fset 'yes-or-no-p 'y-or-n-p)
+(global-set-key (kbd "M-!") #'async-shell-command)
 
 
 ;; Strip UI
