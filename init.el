34c41987ffc7ab88f9625e741eebda3498e4b457
Jonas Westlund <jonve547@student.liu.se>
2017-06-06 01:26:03 +0200

    Tweaked doom-one theme

--- init.el
+++ init.el
@@ -237,7 +237,7 @@ Example output:
 	  (t                :inherit linum)))
        `(git-gutter:modified ((t (:foreground "#ECBE7B"))))
        `(git-gutter-fr:modified ((t (:foreground "#ECBE7B"))))
-       `(font-lock-preprocessor-face ((t (:foreground "#C8C8C8" :bold t))))
+       `(font-lock-preprocessor-face ((t (:foreground "#DA8548" :bold t))))
        `(nlinum-relative-current-face ((t (:foreground "#46D9FF" :bold t))))
        `(font-lock-variable-name-face ((t (:foreground "#DFDFDF"))))))
     (when (member 'doom-tomorrow-night custom-enabled-themes)
