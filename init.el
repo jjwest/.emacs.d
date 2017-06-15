7fb5572c23b1bd676bb7da032caf8e2944c65746
Jonas Westlund <jonve547@student.liu.se>
2017-06-15 22:45:53 +0200

    Removed evil-goggles

--- init.el
+++ init.el
@@ -354,13 +354,6 @@ Example output:
   :ensure t
   :config (global-evil-visualstar-mode))
 
-(use-package evil-goggles
-  :ensure t
-  :config
-  (setq evil-goggles-duration 0.05)
-  (evil-goggles-use-diff-faces)
-  (evil-goggles-mode))
-
 (use-package yasnippet
   :ensure t
   :defer t
